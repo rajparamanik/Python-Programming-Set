5c1f00cced48db81530942c6e4f3894c25a2d65c
Raj Paramanik <rajparamanik42@gmail.com>
2026-08-28 20:42:29 +0530

    add a variable file for understanding the python veriable

---
+++ Besics/Variables.py
@@ -0,0 +1,29 @@
+# There are no variables in Python. Instead, Python uses names to refer to objects.
+#  A name is a reference to an object, and the object can be of any type (e.g., integer, string, list, etc.).
+
+Name1 = "Hello, World!"  # This is a string object
+print(Name1)  # This will print the string object
+
+Number1 = 42  # This is an integer object
+print(Number1)  # This will print the integer object
+
+Boolean1 = True  # This is a boolean object
+print(Boolean1)  # This will print the boolean object
+
+float1 = 3.14  # This is a float object
+print(float1)  # This will print the float object
+
+List1 = [1, 2, 3, 4, 5]  # This is a list object
+print(List1)  # This will print the list object
+
+dict1 = {"Name": "Raj", "Roll": 20}  # This is a dictionary object
+print(dict1)  # This will print the dictionary object 
+
+set1 = {1, 2, 3, 4, 5}  # This is a set object
+print(set1)  # This will print the set object
+
+tuple1 = (1, 2, 3, 4, 5)  # This is a tuple object
+print(tuple1)  # This will print the tuple object
+
+
+
